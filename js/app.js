436926bb8cdc243654538129b2f8abc23b141efd
makovskaia <makovskayamd@gmail.com>
2016-04-04 21:21:41 +0300

    display additional info card when small pokemon is clicked, add it's styling

--- js/app.js
+++ js/app.js
@@ -8,10 +8,73 @@ $.ajax({
         typesHtml += '<span class="'+element.name+'">'+element.name+'</span>';
       }
   pokemon.types.forEach(getTypes);
-      var html = '<div class="col-xs-4 card"><div><img class="img-responsive" src="http://pokeapi.co/media/img/'+pokemon.national_id+'.png"/><h3>'+pokemon.name+'</h3><p>'+typesHtml+'</p></div></div>';
-      $("#cards").append(html);
+      var html = '<div class="col-xs-4 small-card">\
+                    <div class="card" id="'+pokemon.national_id+'">\
+                      <img class="img-responsive" src="http://pokeapi.co/media/img/'+pokemon.national_id+'.png"/>\
+                      <h4>'+pokemon.name+'</h4>\
+                      <p>'+typesHtml+'</p>\
+                    </div>\
+                  </div>';
+      $('#cards').append(html);
     });
   }
 });
-
-
+$(document).ready(function(){
+  $('#cards').on('click', '.card', function(){
+    //console.log('hi ');
+        $.ajax({
+      url: 'http://pokeapi.co/api/v1/pokemon/'+$(this).attr('id'),
+      dataType: 'json',
+      success: function(data){
+        var typesHtml = "";
+        function getTypes(element) {
+          typesHtml += '<span class="'+element.name+'">'+element.name+'</span>';
+      }
+        data.types.forEach(getTypes);
+        var html = '<div class="selected-pokemon">\
+                      <img class="img-responsive" src="http://pokeapi.co/media/img/'+data.national_id+'.png"/>\
+                      <h3>'+data.name+' #'+data.national_id+'</h3>\
+                      <table>\
+                        <tr>\
+                          <td>Type</td>\
+                          <td>'+typesHtml+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>Attack</td>\
+                          <td>'+data.attack+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>Defense  </td>\
+                          <td>'+data.defense+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>HP</td>\
+                          <td>'+data.hp+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>SP Attack</td>\
+                          <td>'+data.sp_atk+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>SP Defense</td>\
+                          <td>'+data.sp_def+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>Speed</td>\
+                          <td>'+data.speed+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>Weight</td>\
+                          <td>'+data.weight+'</td>\
+                        </tr>\
+                        <tr>\
+                          <td>Total Moves</td>\
+                          <td>'+data.moves.length+'</td>\
+                        </tr>\
+                      </table>\
+                    </div>';
+        $("#selected-card").html(html);
+      }
+    });
+  });
+});
